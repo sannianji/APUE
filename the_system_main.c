586ab1711bbc0b26317a1157a79abb9e76df7897
sannianji <sannianjisansan@gmail.com>
2017-07-01 14:30:15 +0800

    system

---
+++ the_system_main.c
@@ -0,0 +1,20 @@
+#include "apue.h"
+#include <sys/wait.h>
+int main(void)
+{
+	int status;
+	if((status=system("date"))<0)
+		err_sys("systerm()");
+	
+	pr_exit(status);
+		
+	if((status=system("nosuchcommand"))<0)
+		err_sys("systerm()");
+	pr_exit(status);
+	
+	if((status=system("who; exit 44"))<0)
+		err_sys("system");
+	pr_exit(status);
+	
+	exit(0);
+}
