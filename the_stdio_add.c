238d829888e4635de3a845dc35c2373733c81d6f
sannianji <sannianjisansan@gmail.com>
2017-12-10 20:44:34 +0800

    ha

---
+++ the_stdio_add.c
@@ -0,0 +1,24 @@
+#include "apue.h"
+
+int main(void)
+{
+	int int1,int2;
+	char line[MAXLINE];
+	
+	if(setvbuf(stdin,NULL,_IOLBF,0)!=0)
+		err_sys("setvbuf stdin");
+	if(setvbuf(stdout,NULL,_IOLBF,0)!=0)
+		err_sys("setvbuf stdout");
+	while(fgets(line,MAXLINE,stdin)!=NULL)
+	{
+		if(sscanf(line,"%d%d",&int1,&int2)==2)
+		{
+			if(printf("%d",int1+int2)==EOF)
+				err_sys("printf error");
+		}else{
+		if(printf("invalid args\n")==EOF)
+			err_sys("printf error");
+		}
+	}
+	exit(0);
+}
